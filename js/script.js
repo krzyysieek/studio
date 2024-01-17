4cb9c22f842857f16cbebe438752268a60a36d42
Krzysiek <kbrzezawski@gmail.com>
2024-01-17 22:57:45 +0100

    lastver

--- js/script.js
+++ js/script.js
@@ -1,8 +1,16 @@
 const navItems = document.querySelectorAll('.nav-link')
 const navArea = document.querySelector('.navbar-collapse')
 const navBar = document.querySelector('.navbar-toggler')
+const cookieBox = document.querySelector('.cookies-box')
+const cookieBtn = document.querySelector('.accept-btn')
 
+const executeCodes = () => {
+    cookieBox.classList.add('show')
+};
 
+const closeCookies = () => {
+    cookieBox.classList.remove('show')
+}
 
 const closeNav = () => {
     navArea.classList.remove('show')
@@ -13,3 +21,6 @@ const closeNav = () => {
 navItems.forEach(navItem => {
     navItem.addEventListener('click', closeNav);
   });
+
+  window.addEventListener("load", executeCodes)
+  cookieBtn.addEventListener('click', closeCookies)
